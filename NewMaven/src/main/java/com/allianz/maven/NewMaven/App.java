ee658f7cc69639aadca814ce5fa1fb08dddcc119
kkshafeeque <extern.kandy_shafeeque@allianz.de>
2018-04-09 14:37:26 +0200

    introduced spaces

--- NewMaven/src/main/java/com/allianz/maven/NewMaven/App.java
+++ NewMaven/src/main/java/com/allianz/maven/NewMaven/App.java
@@ -6,7 +6,8 @@ package com.allianz.maven.NewMaven;
  */
 public class App 
 {
-    public static void main( String[] args )
+    
+	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
